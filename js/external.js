25359c724ee95d489733cf110e79ed95268dce88
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2022-11-21 13:53:17 -0600

    update changes 1

---
+++ js/external.js
@@ -0,0 +1,8 @@
+
+console.log("Hello from external JavaScript");
+
+alert('Welcome to my Website');
+let favColor= prompt('What is your favorite color');
+let message= "Great," + favColor + " is my favorite color too!";
+console.log('the user entered:' + favColor);
+alert('Great, ' + favColor + ' is my favorite color too!');
