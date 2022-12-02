7defa436cb61ffd1aba69a60bf3800e68311b43f
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2022-12-02 13:46:50 -0600

    commit 3

--- arrays-mini-exercises.js
+++ arrays-mini-exercises.js
@@ -30,14 +30,51 @@ for ( let i = 0; i < 4; i += 1){
 console.log(grocerItems)
 
 
+
+
+console.log(' LOOP NOTES')
 /**
  *
  * Loops Notes
  */
 
+console.log(' #1 EXAMPLE')
 let pies = [ 'apple', 'cherry', 'key lime', 'huckleberry'];
               //0        1          2              3
 for (let i = 0; i < pies.length; i += 1){ // pies.length gives you index #
     console.log(i);
     console.log(pies[i]);
 }
+
+for (let i = pies.length - 1; i >= 0; i -= 1){
+    console.log[i];
+}
+
+
+/** ForEach LOOPS
+ *
+ *  //// SYNTAX/////
+ *
+ *  someArray.forEach(function(element, index, array){
+ *      console.log(element);
+ *      console.log(index);
+ *  }
+ *
+ *  // PROS: simpler Syntax, easier to read
+ *  // CONS: can only increment through entire array
+ *  // USE: when you
+ *
+ *
+ *
+ *  const number = [1, 2, 3, 4, 5];
+ *  function logEvenOddMessage(number){
+ *      let isEvenMsg = ( number % 2 === 0) ? 'even' : 'odd' ;
+ *      console.log(isEvenMsg);
+ *
+ * }
+ *
+ * logEvenOddMessage(numbers[1]);
+ *
+ * numbers.forEach();
+ *
+ */
