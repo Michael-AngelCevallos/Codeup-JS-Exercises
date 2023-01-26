2db6061842e0ee50f166842f47555e6cb1d93627
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-01-26 10:43:51 -0600

    directory fixes

--- external.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello from external JavaScript");
-
-alert('Welcome to my Website');
-let userInput= prompt('What is your favorite color');
-alert("Great," + userInput + " is my favorite color too!");
-console.log('the user entered:' + userInput);
-alert('Great, ' + userInput + ' is my favorite color too!');
---
+++ js/keys.js
@@ -0,0 +1,6 @@
+
+const WEATHER_TOKEN = '9e436e22d5b3d5970cbfaca81c003ea7';
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiYW5nZWw0MTM1IiwiYSI6ImNsZDg0ZXZ0MzByd2Uzb3JpdmE0eHo4NWsifQ.ChU6OuMIqZBpFdtlzVk6wA';
+
+
+
